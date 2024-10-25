c4b97db9770db28759623ee8330301366147c32e
Nico Joun <nicojoun793@gmail.com>
2024-10-25 10:18:42 +0200

    ajout de création d'utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const booksRoutes = require('./routes/books');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -20,5 +21,6 @@ app.use((req, res, next) => {
   });
 
 app.use('/api/books', booksRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
